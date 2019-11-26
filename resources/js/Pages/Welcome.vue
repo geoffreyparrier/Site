a493d9240554704ffff6d80578c6f2785494ce9e
geoffreyparrier <geoffrey.parrier@gmail.com>
2019-11-26 19:48:59 +0100

    Good Base

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,16 @@
+<template>
+    <NavBar></NavBar>
+</template>
+
+<script>
+
+    import NavBar from "../Shared/NavBar";
+
+    export default {
+        components: {
+            NavBar,
+        },
+        props: {
+        },
+    }
+</script>
---
+++ resources/js/Shared/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <main>
+        <header>
+            <div>You are logged in as: userName</div>
+            <nav>
+                <inertia-link href="/">Home</inertia-link>
+                <inertia-link href="/about">About</inertia-link>
+                <inertia-link href="/contact">Contact</inertia-link>
+            </nav>
+        </header>
+        <content>
+            <slot />
+        </content>
+        <footer></footer>
+    </main>
+</template>
+<script>
+    export default {
+        name: 'NavBar'
+    }
+</script>
